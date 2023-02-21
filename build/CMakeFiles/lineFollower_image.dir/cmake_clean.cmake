e6c59859e8814057638e67c6e641d9220771e9d2
sstor012 <sstor012@fiu.edu>
2023-02-21 16:46:12 +0000

    updated filenames

---
+++ build/CMakeFiles/lineFollower_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lineFollower_image.dir/center_image.cpp.o"
+  "lineFollower_image"
+  "lineFollower_image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lineFollower_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/lineFollower_video.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lineFollower_video.dir/center_video.cpp.o"
+  "lineFollower_video"
+  "lineFollower_video.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lineFollower_video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
